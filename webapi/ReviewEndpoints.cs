58310683983ddc67a09dec9f85f5de74f9109424
Aiwar4s <vaitkusaivaras5@gmail.com>
2023-12-18 02:26:21 +0200

    removed async from review dto creator

--- webapi/ReviewEndpoints.cs
+++ webapi/ReviewEndpoints.cs
@@ -38,7 +38,7 @@ namespace webapi
                     previousPageLink, nextPageLink);
 
                 httpContext.Response.Headers.Add("Pagination", JsonSerializer.Serialize(paginationMetadata));
-                return pagedList.Select(async review => await CreateReviewDto(new ReviewDtoTemp(review.Id, review.Rating, review.Description, review.UserId), dbContext));
+                return pagedList.Select(review => CreateReviewDto(new ReviewDtoTemp(review.Id, review.Rating, review.Description, review.UserId), dbContext));
             }).WithName("GetReviews");
 
             reviewsGroup.MapGet("reviews/{reviewId}", async ([AsParameters] GetReviewParameters parameters, UserManager<User> userManager) =>
@@ -116,7 +116,7 @@ namespace webapi
             }).WithName("RemoveReview");
         }
 
-        static async Task<ReviewDto> CreateReviewDto(ReviewDtoTemp review, TripDbContext dbContext)
+        static ReviewDto CreateReviewDto(ReviewDtoTemp review, TripDbContext dbContext)
         {
             var user=dbContext.Users.FirstOrDefault(u => u.Id == review.ReviewerId);
             return new ReviewDto(review.Id, review.Rating, review.Description, review.ReviewerId, user.UserName);
