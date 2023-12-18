e786952f77d65e212597a41026304a55d7f8b61d
Aiwar4s <vaitkusaivaras5@gmail.com>
2023-12-18 02:02:00 +0200

    try to fix review dto

--- webapi/Data/Entities/Review.cs
+++ webapi/Data/Entities/Review.cs
@@ -18,6 +18,7 @@ namespace webapi.Data.Entities
 }
 
 public record GetReviewParameters(int driverId, int tripId, int reviewId, TripDbContext dbContext);
-public record ReviewDto(int Id, int Rating, string? Description, string ReviewerId);
+public record ReviewDtoTemp(int Id, int Rating, string? Description, string ReviewerId);
+public record ReviewDto(int Id, int Rating, string? Description, string ReviewerId, string Reviewer);
 public record CreateReviewDto(int Rating, string? Description);
 public record UpdateReviewDto(int Rating, string? Description);
--- webapi/ReviewEndpoints.cs
+++ webapi/ReviewEndpoints.cs
@@ -38,7 +38,7 @@ namespace webapi
                     previousPageLink, nextPageLink);
 
                 httpContext.Response.Headers.Add("Pagination", JsonSerializer.Serialize(paginationMetadata));
-                return pagedList.Select(async review => new ReviewDto(review.Id, review.Rating, review.Description, review.UserId));
+                return pagedList.Select(review => CreateReviewDto(new ReviewDtoTemp(review.Id, review.Rating, review.Description, review.UserId), userManager));
             }).WithName("GetReviews");
 
             reviewsGroup.MapGet("reviews/{reviewId}", async ([AsParameters] GetReviewParameters parameters, UserManager<User> userManager) =>
@@ -50,7 +50,7 @@ namespace webapi
                     return Results.NotFound();
                 }
                 var user = await userManager.FindByIdAsync(review.UserId);
-                return Results.Ok(new ReviewDto(review.Id, review.Rating, review.Description, review.UserId));
+                return Results.Ok(new ReviewDto(review.Id, review.Rating, review.Description, review.UserId, user.UserName));
             }).WithName("GetReview");
 
             reviewsGroup.MapPost("reviews", [Authorize(Roles = UserRoles.BasicUser)] async ([Validate] CreateReviewDto createReviewDto, int driverId, int tripId, HttpContext httpContext, LinkGenerator linkGenerator, TripDbContext dbContext, UserManager<User> userManager) =>
@@ -72,7 +72,7 @@ namespace webapi
 
                 var links = CreateLinks(review.Id, httpContext, linkGenerator);
                 var user = await userManager.FindByIdAsync(review.UserId);
-                var reviewDto = new ReviewDto(review.Id, review.Rating, review.Description, review.UserId);
+                var reviewDto = new ReviewDto(review.Id, review.Rating, review.Description, review.UserId, user.UserName);
                 var resource = new ResourceDto<ReviewDto>(reviewDto, links.ToArray());
 
                 return Results.Created($"/api/drivers/{driverId}/trips/{tripId}/reviews/{review.Id}", resource);
@@ -95,7 +95,7 @@ namespace webapi
                 parameters.dbContext.Update(review);
                 await parameters.dbContext.SaveChangesAsync();
                 var user = await userManager.FindByIdAsync(review.UserId);
-                return Results.Ok(new ReviewDto(review.Id, review.Rating, review.Description, review.UserId));
+                return Results.Ok(new ReviewDto(review.Id, review.Rating, review.Description, review.UserId, user.UserName));
             }).WithName("EditReview");
 
             reviewsGroup.MapDelete("reviews/{reviewId}", [Authorize(Roles = UserRoles.BasicUser)] async ([AsParameters] GetReviewParameters parameters, HttpContext httpContext) =>
@@ -114,6 +114,12 @@ namespace webapi
                 await parameters.dbContext.SaveChangesAsync();
                 return Results.NoContent();
             }).WithName("RemoveReview");
+        }
+
+        static async Task<ReviewDto> CreateReviewDto(ReviewDtoTemp review, UserManager<User> userManager)
+        {
+            var username=(await userManager.FindByIdAsync(review.ReviewerId)).UserName;
+            return new ReviewDto(review.Id, review.Rating, review.Description, review.ReviewerId, username);
         }
         static IEnumerable<LinkDto> CreateLinks(int reviewId, HttpContext httpContext, LinkGenerator linkGenerator)
         {
